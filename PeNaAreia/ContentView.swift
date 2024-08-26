a009ba670d55a82624e05deb6b36fb20f13d5231
Miriam Mendes <miriam.rayanemendes@ufpe.br>
2024-08-26 10:46:31 -0300

    third test commit

--- PeNaAreia/ContentView.swift
+++ PeNaAreia/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Cãoculadora?")
+            Text("Cãoculadora?!?!")
         }
         .padding()
     }
