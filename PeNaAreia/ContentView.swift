d8474c6892a429208b163a2a3162a91cf407075b
Miriam Mendes <miriam.rayanemendes@ufpe.br>
2024-08-26 10:21:46 -0300

    Initial Commit

---
+++ PeNaAreia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PeNaAreia
+//
+//  Created by Miriam Mendes on 26/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeNaAreia/PeNaAreiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeNaAreiaApp.swift
+//  PeNaAreia
+//
+//  Created by Miriam Mendes on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PeNaAreiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
