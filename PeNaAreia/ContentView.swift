1ece11331298b82d9d6c3a7e584f7ce6a2982d09
Miri Mendes <108680161+miriamrms@users.noreply.github.com>
2024-08-26 10:49:27 -0300

    Update ContentView.swift

--- PeNaAreia/ContentView.swift
+++ PeNaAreia/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Cãoculadora?")
+            Text("Pé na areia?...")
         }
         .padding()
     }
