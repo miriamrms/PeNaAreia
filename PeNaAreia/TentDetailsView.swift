0ebb815ff6a541556045b6b4d62e96979b93decb
Marina-Paixao <marina.paixao@ufpe.br>
2024-08-27 12:23:36 -0300

    Adicionei alguns icones, de voltar, like, lotação e textos na tela

--- PeNaAreia/TentDetailsView.swift
+++ PeNaAreia/TentDetailsView.swift
@@ -10,22 +10,49 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            ZStack {
+                Color("backgroundsand")
+                
+                VStack {
+                    Image("fotoBarracaTemp")
+                        .frame(width: 393.0, height: 291.0)
+                    Spacer()
+                }
+                .ignoresSafeArea()
+                
+                VStack(alignment: .center) {
+                    Spacer()
+                    Image("BackgroundBarracas")
+                        .resizable()
+                    .scaledToFill()
+                    .frame(height: 596.0)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                }
+                .ignoresSafeArea()
+            }
             VStack {
-                Image("fotoBarracaTemp")
-                    .frame(width: 393.0, height: 291.0)
-                Spacer()
+                Image("back.ic")
+                
+                Image("like.ic")
+                
+                HStack {
+                    Text("Nome da Barraca")
+                        .font(.system(size: 34))
+                        .fontDesign(.rounded)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.darkblue)
+                    
+                    VStack {
+                        Image("lotacaoBaixa.ic")
+                        
+                        Text("Lotação há 2min")
+                            .font(.system(size: 12))
+                            .fontDesign(.rounded)
+                            .fontWeight(.light)
+                            .foregroundColor(Color.darkblue)
+                    }
+                }
             }
-            .ignoresSafeArea()
-            
-            VStack(alignment: .center) {
-                Spacer()
-                Image("BackgroundBarracas")
-                    .resizable()
-                .scaledToFill()
-                .frame(height: 596.0)
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-            }
-            .ignoresSafeArea()
             
         }
     }
