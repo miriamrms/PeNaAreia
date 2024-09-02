b28f1f9421dcd0c71fcf186748f54a21d16f7d81
stnrfzz <stonerfuzz00@gmail.com>
2024-09-02 09:43:56 -0300

    Fix commit |Extension for array in appstorage

---
+++ PeNaAreia/Tent.swift
@@ -0,0 +1,16 @@
+//
+//  Tent.swift
+//  PeNaAreia
+//
+//  Created by Caio Silva on 30/08/24.
+//
+
+import Foundation
+
+class Tent {
+    var id: Int
+    
+    init(id: Int) {
+        self.id = id
+    }
+}
