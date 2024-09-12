bc3a1a205c2ee5e007bf8994f84ad5d36684161d
Marina-Paixao <marina.paixao@ufpe.br>
2024-09-12 10:35:07 -0300

    Adicionei LaunchScreen, e mudei o fluxo da pasta PeNaAreiaApp para aparecer a launchscreen

---
+++ PeNaAreia/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  PeNaAreia
+//
+//  Created by Marina Meirelles Paixão on 12/09/24.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
