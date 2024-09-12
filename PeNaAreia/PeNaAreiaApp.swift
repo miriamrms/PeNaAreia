ed93e0ec8ad8bd43b7ee690a518ae019c38397a7
Marina-Paixao <marina.paixao@ufpe.br>
2024-09-12 10:44:29 -0300

    Recoloquei launchscreen e alterações no penaareiaapp, porque sumiram quando eu dei pull

--- PeNaAreia/PeNaAreiaApp.swift
+++ PeNaAreia/PeNaAreiaApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct PeNaAreiaApp: App {
+    @State private var showAnimatedWave = true // Controla qual view será exibida
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if showAnimatedWave {
+                AnimatedWaveView()
+                    .onAppear {
+                        // Tempo de transição pra ContentView
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                            withAnimation {
+                                showAnimatedWave = false
+                            }
+                        }
+                    }
+            } else {
+                ContentView() // Vai pra ContentView depois que o AnimatedWaveView aparece
+            }
         }
     }
 }
